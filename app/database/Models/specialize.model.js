03624362866537cf42425f80ead5848f6e28d03c
safaagouda <126277649+safaagouda@users.noreply.github.com>
2023-03-06 19:53:20 +0200

    Add files via upload

---
+++ app/database/Models/specialize.model.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+var jwt = require("jsonwebtoken");
+const specializeSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      maxLength: 255,
+      unique: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "user",
+    },
+    img: Array,
+  },
+  { timestamps: true }
+);
+specializeSchema.virtual("specialize", {
+  ref: "specialize",
+  localField: "_id",
+  foreignField: "specializeId",
+});
+const specialize = mongoose.model("specialize", specializeSchema);
+module.exports = specialize;
