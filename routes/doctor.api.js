c27e5a8b795aab86151591379c0ab38ae86993a5
safaagouda <126277649+safaagouda@users.noreply.github.com>
2023-03-07 21:37:49 +0200

    Update doctor.api.js

--- routes/doctor.api.js
+++ routes/doctor.api.js
@@ -3,12 +3,7 @@ const auth = require("../middleware/auth");
 const doctorController = require("../app/controller/doctor.controller");
 const upload1 = require("../middleware/fileUpload1");
 
-router.post('/registerAsDoctor',upload1.single('img'),doctorController.applyNewDoctor)
-router.post("/login", doctorController.login);
-router.get("/logout", auth, doctorController.logout);
-router.post('/forgotPass',doctorController.forgot_password)
-router.post('/passwordVerify',doctorController.passwordVerify)
-router.delete("/all/:id", auth, doctorController.deleteAccount);
-router.patch("/all/:id", doctorController.editUser);
-router.patch("/changePass", doctorController.editPass);
+router.post('/all/:id',upload1.single('img'),doctorController.practicesLicence)
+router.post('/addLocation',auth,doctorController.addDoctorData)
+router.post('/clinicImgs',upload1.array('img',5),doctorController.addImages)
 module.exports = router;
